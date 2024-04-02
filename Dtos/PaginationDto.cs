0825f52efa62ca8ed612935addb79d65e0ed808e
robin.correa <robin.correa21@gmail.com>
2024-04-02 12:44:43 +0800

    Simplify CORS implementation.

--- Dtos/PaginationDto.cs
+++ Dtos/PaginationDto.cs
@@ -8,21 +8,21 @@ namespace web_api_remit_rocket.Helpers
 
         public string? firstPageUrl { get; set; } = null;
 
-        public int? from { get; set; }
+        public int? from { get; set; } = null;
 
-        public int lastPage { get; set; }
+        public int? lastPage { get; set; } = null;
 
         public string? lastPageUrl { get; set; } = null;
 
-        public string? nextPageUrl { get; set; }
+        public string? nextPageUrl { get; set; } = null;
 
         public string? path { get; set; } = null;
 
         public int perPage { get; set; }
 
-        public string? previousPageUrl { get; set; }
+        public string? previousPageUrl { get; set; } = null;
 
-        public int? to { get; set; }
+        public int? to { get; set; } = null;
         
         public int total { get; set; }
     }
--- Program.cs
+++ Program.cs
@@ -17,15 +17,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll",
-        b => b.AllowAnyHeader()
-        .AllowAnyOrigin()
-        .AllowAnyMethod());
-});
-
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,7 +28,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors("AllowAll");
+app.UseCors(options =>
+{
+    options.AllowAnyHeader();
+    options.AllowAnyOrigin();
+    options.AllowAnyMethod();
+});
 
 app.UseAuthorization();
 
